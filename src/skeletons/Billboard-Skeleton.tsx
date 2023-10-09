a5b22aa2111600e6a33ae33f4abd1d737da16ed6
Hibathulla <hibathullacm@gmail.com>
2023-10-09 22:03:28 +0530

    refactor(skeleton): skeleton bug fix

--- src/skeletons/Billboard-Skeleton.tsx
+++ src/skeletons/Billboard-Skeleton.tsx
@@ -8,9 +8,7 @@ const BillboardSkeleton = () => {
         className={
           "rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
         }
-      >
-        {/* <Image src={Banner2} alt="" width={500} height={500} /> */}
-      </Skeleton>
+      />
     </div>
   );
 };
